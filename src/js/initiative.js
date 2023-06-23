2632c14ad628f11bda18ce192ffd6215f18dacd2
Dustin Whisman <whisman.dustin@gmail.com>
2023-06-23 11:20:58 -0600

    refactor: break script into smaller functions

--- src/js/initiative.js
+++ src/js/initiative.js
@@ -1,4 +1,4 @@
-const updateInitiativeOrder = (focusOnUpdate = false) => {
+const renderInitiativeOrder = (focusOnUpdate = false) => {
   let initiativeOrder = [];
   const rawInitiativeOrder = localStorage.getItem('initiative-order');
   if (rawInitiativeOrder) {
@@ -9,16 +9,18 @@ const updateInitiativeOrder = (focusOnUpdate = false) => {
   if (initiativeList) {
     initiativeList.innerHTML = initiativeOrder
       .map(
-        (item) => `<li value="${item.initiative}">
-    <div class="cmp-initiative__list-item">
-      <span class="cmp-initiative__name">
-        ${item.characterName}
-      </span>
-      <button type="button" class="cmp-initiative__delete" data-character-name="${item.characterName}" data-initiative="${item.initiative}">
-        Delete
-      </button>
-    </div>
-  </li>`,
+        (item) => `
+<li value="${item.initiative}">
+  <div class="cmp-initiative__list-item">
+    <span class="cmp-initiative__name">
+      ${item.characterName}
+    </span>
+    <button type="button" class="cmp-initiative__delete" data-character-name="${item.characterName}" data-initiative="${item.initiative}">
+      Delete
+    </button>
+  </div>
+</li>
+`,
       )
       .join('');
 
@@ -26,6 +28,67 @@ const updateInitiativeOrder = (focusOnUpdate = false) => {
       initiativeList.focus();
     }
   }
+};
+
+const resetErrorState = (input) => {
+  input.removeAttribute('aria-invalid');
+  input.removeAttribute('aria-describedby');
+};
+
+const showErrorMessages = (errorList, formName, containerSelector) => {
+  const ul = document.createElement('ul');
+  ul.id = `${formName}-errors`;
+  ul.setAttribute('tabindex', '-1');
+  ul.classList.add('cmp-error-list');
+
+  errorList.forEach((error) => {
+    const li = document.createElement('li');
+    li.id = `${error.fieldName}-field-error`;
+    li.textContent = error.message;
+    ul.appendChild(li);
+
+    const input = document.querySelector(`[name="${error.fieldName}"]`);
+    input?.setAttribute('aria-invalid', 'true');
+    input?.setAttribute('aria-describedby', `${error.fieldName}-field-error`);
+  });
+
+  const container = document.querySelector(containerSelector);
+  container?.prepend(ul);
+  ul.focus();
+};
+
+const setInitiativeOrder = ({ initiative, characterName }) => {
+  let initiativeOrder;
+  const rawInitiativeOrder = localStorage.getItem('initiative-order');
+  if (rawInitiativeOrder) {
+    initiativeOrder = JSON.parse(rawInitiativeOrder);
+    initiativeOrder = [
+      ...initiativeOrder,
+      {
+        initiative,
+        characterName,
+      },
+    ].sort((a, b) => {
+      if (a.initiative > b.initiative) {
+        return -1;
+      }
+
+      if (a.initiative < b.initiative) {
+        return 1;
+      }
+
+      return 0;
+    });
+  } else {
+    initiativeOrder = [
+      {
+        initiative,
+        characterName,
+      },
+    ];
+  }
+
+  localStorage.setItem('initiative-order', JSON.stringify(initiativeOrder));
 };
 
 const bindInitiativeFormListener = () => {
@@ -38,8 +101,7 @@ const bindInitiativeFormListener = () => {
     const errorList = [];
 
     const initiativeInput = event.target.elements.initiative;
-    initiativeInput.removeAttribute('aria-invalid');
-    initiativeInput.removeAttribute('aria-describedby');
+    resetErrorState(initiativeInput);
     const rawInitiative = initiativeInput.value;
     if (!rawInitiative) {
       errorList.push({
@@ -57,8 +119,7 @@ const bindInitiativeFormListener = () => {
     }
 
     const characterNameInput = event.target.elements['character-name'];
-    characterNameInput.removeAttribute('aria-invalid');
-    characterNameInput.removeAttribute('aria-describedby');
+    resetErrorState(characterNameInput);
     const characterName = characterNameInput.value;
     if (!characterName) {
       errorList.push({
@@ -68,67 +129,20 @@ const bindInitiativeFormListener = () => {
     }
 
     if (errorList.length) {
-      const ul = document.createElement('ul');
-      ul.id = 'initiative-form-errors';
-      ul.setAttribute('tabindex', '-1');
-      ul.classList.add('cmp-initiative__error-list');
-
-      errorList.forEach((error) => {
-        const li = document.createElement('li');
-        li.id = `${error.fieldName}-field-error`;
-        li.textContent = error.message;
-        ul.appendChild(li);
-
-        const input = document.querySelector(`[name="${error.fieldName}"]`);
-        input?.setAttribute('aria-invalid', 'true');
-        input?.setAttribute(
-          'aria-describedby',
-          `${error.fieldName}-field-error`,
-        );
-      });
-
-      const container = document.querySelector('.cmp-initiative__container');
-      container?.prepend(ul);
-      ul.focus();
+      showErrorMessages(
+        errorList,
+        'initiative-form',
+        '.cmp-initiative__container',
+      );
       return;
     }
 
-    let initiativeOrder;
-    const rawInitiativeOrder = localStorage.getItem('initiative-order');
-    if (rawInitiativeOrder) {
-      initiativeOrder = JSON.parse(rawInitiativeOrder);
-      initiativeOrder = [
-        ...initiativeOrder,
-        {
-          initiative,
-          characterName,
-        },
-      ].sort((a, b) => {
-        if (a.initiative > b.initiative) {
-          return -1;
-        }
-
-        if (a.initiative < b.initiative) {
-          return 1;
-        }
-
-        return 0;
-      });
-    } else {
-      initiativeOrder = [
-        {
-          initiative,
-          characterName,
-        },
-      ];
-    }
-
-    localStorage.setItem('initiative-order', JSON.stringify(initiativeOrder));
-    updateInitiativeOrder(true);
+    setInitiativeOrder({ initiative, characterName });
+    renderInitiativeOrder(true);
   });
 };
 
 (() => {
   bindInitiativeFormListener();
-  updateInitiativeOrder();
+  renderInitiativeOrder();
 })();
